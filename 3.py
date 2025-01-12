16e900f8800c820e586ace586953dac26cf2f2f0
crallt <cavabanga14741@mail.ru>
2025-01-13 00:56:27 +0700

    Обновил file1.txt

--- 3.py
+++ 3.py
@@ -82,7 +82,7 @@ def display_hanagman(tries):
 
 
 
-def found(text, char, w, tries):
+def found(text, char, w, tries, z_z):
     start = 0
     z = 0
     indexes = []
@@ -92,7 +92,7 @@ def found(text, char, w, tries):
             if z == 0:
                 tries = tries - 1
                 print(display_hanagman(tries))
-                print(';s')
+                #print('Вы проиграли, загаданое слово: ', l)
             for i in indexes:
                 f = l[i]
                 w = w[:i] + f + w[i+1:] 
@@ -118,52 +118,79 @@ def get_word():
     return l
 
 l = get_word()
+print(l)
 ss = False
 tries = 6
 
 w = '_' * len(l)
+z_z = 0
 
-while ss == False:
-    z = input()
-
-    if uu(z) == False:
-        print('введен неверный знак')
-        continue         
-
-    if len(z) == 1:
-        if z in w:
-            print('введенная вами буква уже есть, введите другую')
-            continue
-
-        if w == l:
-            print("Вы победили!")
-            break
-        else:
-            indexes = found(l, z,w, tries)
-            w, tries = indexes
-            if tries == 0:
-                print('Вы проиграли')
-                break
-            print(tries)
-            print(w, 'ыы')
-
+def play(w,l,tries,z_z):
+    if z_z > 0:
+        l = get_word()
+        print(l)
     else:
-        if z == l:
-            print('Ответ верный!')
-            break
+        False   
+
+    while ss == False:
+        z = input("Введите букву или слово целиком: \n")
+
+        if uu(z) == False:
+            print('введен неверный знак')
+            continue         
+
+        if len(z) == 1:
+            if z in w:
+                print('введенная вами буква уже есть, введите другую')
+                continue
+
+            if w == l:
+                print("Вы победили!")
+                z_z= z_z +1
+                tt = input("Хотите сыграть еще раз? Да /n")
+                if tt == 'Да' or tt == 'да':
+                    play(w,l, tries, z_z)
+                break
+            else:
+                indexes = found(l, z,w, tries, z_z)
+                w, tries = indexes
+                if tries == 0:
+                    print('Вы проиграли загаданное слово: ', l)
+                    z_z= z_z +1
+                    tt = input("Хотите сыграть еще раз? Да \n")
+                    if tt == 'Да' or tt == 'да':
+                        play(w,l, tries, z_z)
+                    else:
+                        break
+                print('Количество оставшихся жизней: ', tries)
+                print(w)
+
         else:
-            tries = tries - 1
-            display_hanagman(tries)
+            if z == l:
+                print('Ответ верный!')
+                z_z= z_z +1
+                tt = input("Хотите сыграть еще раз? Да \n")
+                if tt == 'Да' or tt == 'да':
+                    play(w,l, tries, z_z)
+                else:
+                    break
+            else:
+                tries = tries - 1
+
+                print('Количество оставшихся жизней: ', tries)
+                #print('fffdfd')
+                print(display_hanagman(tries))
+                if tries == 0:
+                    print('Вы проиграли загаданное слово: ', l)
+                    z_z= z_z +1
+                    tt = input("Хотите сыграть еще раз? Да \n")
+                    if tt == 'Да' or tt == 'да':
+                        play(w,l, tries, z_z)
+                    else:
+                        break
 
 
+play(w,l, tries, z_z)
 
 
-
-
-
-
-
-w = indexes
-
-print(w) 
 
